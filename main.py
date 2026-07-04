fc9288804c252e386b04c2cc398ea7dba99cb04a
OnuSike <117122235+OnuSike@users.noreply.github.com>
2026-07-04 21:45:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from passiveSnooping import init_db, start_in_thread
 from app import app
+from config import HOST, PORT
 
 if __name__ == "__main__":
     init_db()
     start_in_thread()
-    app.run(debug=False, port=5000)
+    app.run(debug=False, host=HOST, port=PORT)
