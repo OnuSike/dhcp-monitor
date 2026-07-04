780e2a95829864cd78b2c8feda39f11a51958ae9
OnuSike <117122235+OnuSike@users.noreply.github.com>
2026-07-04 21:44:54 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # None for auto-detection, or set manually
-TRUSTED_GATEWAY = "None"  # e.g. "192.168.1.1"
-TRUSTED_DHCP_SERVER = "None"  # e.g. "192.168.1.1"
+TRUSTED_GATEWAY = "192.168.1.1"  # e.g. "192.168.1.1"
+TRUSTED_DHCP_SERVER = "192.168.1.1"  # e.g. "192.168.1.1"
 
 INTERFACE = None  # e.g. "\\Device\\NPF_{1E514016-1566-4FDE-888C-281DC2F2883F}" on Windows, "eth0" on Linux
 # leave as 'None' for Scapy auto-detection(conf.iface) - not recommended
@@ -8,3 +8,7 @@ INTERFACE = None  # e.g. "\\Device\\NPF_{1E514016-1566-4FDE-888C-281DC2F2883F}" 
 STARVATION_THRESHOLD = 10
 STARVATION_WINDOW = 5
 STARVATION_COOLDOWN = 30
+
+# Web Server Host
+HOST = "0.0.0.0" # "0.0.0.0" listens on all interfaces, or set to a specific IP e.g. "192.168.1.5"
+PORT = 5000
