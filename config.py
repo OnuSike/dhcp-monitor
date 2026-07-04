ac47fa955d867034693861c4b3ad418c29f08ddf
OnuSike <117122235+OnuSike@users.noreply.github.com>
2026-07-04 21:09:52 +0300

    Update config.py
    Set default values to "None"

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # None for auto-detection, or set manually
-TRUSTED_GATEWAY = "192.168.1.1"  # e.g. "192.168.1.1"
-TRUSTED_DHCP_SERVER = "192.168.1.1"  # e.g. "192.168.1.1"
+TRUSTED_GATEWAY = "None"  # e.g. "192.168.1.1"
+TRUSTED_DHCP_SERVER = "None"  # e.g. "192.168.1.1"
 
 INTERFACE = None  # e.g. "\\Device\\NPF_{1E514016-1566-4FDE-888C-281DC2F2883F}" on Windows, "eth0" on Linux
 # leave as 'None' for Scapy auto-detection(conf.iface) - not recommended
